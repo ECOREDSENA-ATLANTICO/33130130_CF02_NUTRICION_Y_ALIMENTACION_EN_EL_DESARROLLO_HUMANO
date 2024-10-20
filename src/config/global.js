be01d2f2cfae4b91f36bfa3094835f308391b932
Leoska89 <leoska890@gmail.com>
2024-10-20 18:52:27 -0500

    up

--- src/config/global.js
+++ src/config/global.js
@@ -104,32 +104,220 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
-    },
-    {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Concejo de alimentación para prevenir enfermedades.',
+      referencia: 'Ecosistema recursos SENA [Video]. YouTube. ',
+      tipo: 'Video',
+      link: 'https://youtu.be/1gQgdQHYYDU?si=P6Nap2toNJ5JAjO1',
     },
   ],
+
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Absorción:',
+      significado:
+        'es el proceso por el cual los nutrientes se mueven desde el lumen intestinal hasta la sangre, dando como resultado la absorción de nutrientes en el organismo.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Absorción:',
+      significado:
+        'es el proceso por el cual los nutrientes se mueven desde el lumen intestinal hasta la sangre, dando como resultado la absorción de nutrientes en el organismo.',
+    },
+    {
+      termino: 'Amilasa:',
+      significado:
+        'es una enzima hidrolasa que se produce en las gandulas salivares y que tiene la capacidad de sintetizar el almidón para volverlo más simple.',
+    },
+    {
+      termino: 'Absorción:',
+      significado:
+        'es el proceso por el cual los nutrientes se mueven desde el lumen intestinal hasta la sangre, dando como resultado la absorción de nutrientes en el organismo.',
+    },
+    {
+      termino: 'Cáncer:',
+      significado:
+        'es una enfermedad que se caracteriza por la presencia de células anormales en cuerpo, las cuales crecen y se multiplican de manera descontrolada.',
+    },
+    {
+      termino: 'Cáncer:',
+      significado:
+        'es una enfermedad que se caracteriza por la presencia de células anormales en cuerpo, las cuales crecen y se multiplican de manera descontrolada.',
+    },
+    {
+      termino: 'Cerebrósidos:',
+      significado:
+        'es una enfermedad que se caracteriza por la presencia de células anormales en cuerpo, las cuales crecen y se multiplican de manera descontrolada.',
+    },
+    {
+      termino: 'Digestión:',
+      significado:
+        'es un proceso que comprende procesos mecánicos y químicos por medio de los cuales los materiales alimenticios complejos son hidrolizados a formas adecuadas, para poder ser absorbidos por la pared de la mucosa y así ser utilizados por el organismo',
+    },
+    {
+      termino: 'Disacáridos:',
+      significado:
+        'están formados por dos monosacáridos y son los azucares más abundantes de los alimentos, los más conocidos son la sacarosa, lactosa y maltosa.',
+    },
+    {
+      termino: 'Enzimas:',
+      significado:
+        'son catalizadores orgánicos de naturaleza proteica que aumentan considerablemente la velocidad de las reacciones.',
+    },
+    {
+      termino: 'Fibra:',
+      significado:
+        'se encuentra y origina en las paredes celulares de los vegetales, los componentes más conocidos son la celulosa, hemicelulosa, pectinas y lignina.',
+    },
+    {
+      termino: 'Glicoproteínas:',
+      significado:
+        'es una proteína que esta unidad a monosacáridos y por lo general tienen mayor número de proteínas.',
+    },
+    {
+      termino: 'Glucolípidos:',
+      significado:
+        'están compuesta de esfingolípidos que es la ceramida más ácida graso, hacen parte de capa lipídica de membrana celular, los glúcidos que los conforman pueden ser galactosa, fructosa, glucosa y manosa.',
+    },
+    {
+      termino: 'Hidratos de carbono:',
+      significado:
+        'los hidratos de carbono o glúcidos son moléculas de carbono, hidrógeno y oxígeno cuya fórmula es (CH2O)n, se clasifican en azúcares simples llamados monosacáridos, disacáridos y polisacáridos.',
+    },
+    {
+      termino: 'Lípidos',
+      significado:
+        'están constituidos por fosfolípidos y colesterol; la función más relevante de estos compuestos es proporcionar energía al metabolismo y dar elementos estructurales a la membrana celular.',
+    },
+    {
+      termino: 'Metabolismo:',
+      significado:
+        'hace referencia a un sinnúmero de reacciones bioquímicas y fisicoquímicas que ocurren en las células y en el organismo. Metabolismo basal (TMB): Hace referencia a la cantidad mínima de energía que el organismo requiere para que el cuerpo se mantenga despierto o en reposo y de esta manera cumplir todas sus actividades vitales como respiración, presión arterial, latidos del corazón, entre otros.',
+    },
+    {
+      termino: 'Minerales:',
+      significado:
+        'son sustancias naturales compuestas por moléculas formadas por átomos de uno o más elementos químicos',
+    },
+    {
+      termino: 'Monosacáridos:',
+      significado:
+        'Son azucares simples compuestos de tres a siete átomos de carbono, en este grupo se encuentra la glucosa y la fructosa.',
+    },
+    {
+      termino: 'Mucina:',
+      significado: 'es una proteína que tiene la capacidad de formar geles.',
+    },
+    {
+      termino: 'Nutrición:',
+      significado:
+        'ciencia que estudia las relaciones entre los nutrientes y el organismo humano a nivel individual y colectivo, en diferentes situaciones fisiológicas y patológicas.',
+    },
+    {
+      termino: 'Nutrientes:',
+      significado:
+        'son compuestos que se encuentran en los alimentos como proteínas, lípidos, glúcidos, minerales y vitaminas; que deben ser suministrados al cuerpo en cantidades adecuadas.',
+    },
+    {
+      termino: 'Pepsina:',
+      significado:
+        'es una enzima que hidroliza las proteínas y se encuentra en el estómago.',
+    },
+    {
+      termino: 'Mucina:',
+      significado: 'es una proteína que tiene la capacidad de formar geles.',
+    },
+    {
+      termino: 'Pepsinógeno:',
+      significado: 'es una proenzima que se encuentra en el estómago.',
+    },
+    {
+      termino: 'Polisacáridos:',
+      significado:
+        'están formados por moléculas de diez o más monosacáridos, se descomponen en moléculas más pequeñas por hidrólisis en el organismo de los seres humanos y tienen la función de mantener reservas de energía.',
+    },
+    {
+      termino: 'Proteínas:',
+      significado:
+        'son sustancias orgánicas que contienen en general una base de elementos químicos como el carbono, el hidrógeno, el oxígeno y nitrógeno, están formadas por cadenas lineales de aminoácidos los cuales están encadenados a enlaces peptídicos que se forman en el grupo carboxilo de un aminoácido.',
+    },
+    {
+      termino: 'Proteoglicanos:',
+      significado:
+        'es una glicoproteína que está unida a un tipo especial de polisacáridos. Triglicéridos: Son grasas naturales de baja densidad, lo cual les da la característica de almacenar energía en el tejido adiposo.',
+    },
+    {
+      termino: 'Vitaminas:',
+      significado:
+        'compuestos orgánicos que el hombre necesita en dosis bajas para que el cuerpo se desarrolle adecuadamente, es necesario ingerirlas pues el organismo no tiene la capacidad de producirlas',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Catherine Shaffer, M. (6 de Noviembre de 2016). <em>News Medical Life Sc iences.</em>',
+      link:
+        'http://www.news-medical.net/health/Types-of-Vitamin-B-(Spanish).aspx ',
+    },
+    {
+      referencia: 'Centro Médico <em>Teknon.</em> (2016).',
+      link:
+        'http://www.teknon.es/es/especialidades/nogues-boqueras-raquel/nutricion-clinica.',
+    },
+    {
+      referencia:
+        'Corine, H. (182). Fundamentos de Nutrición. México: Continental.',
+    },
+    {
+      referencia:
+        'Elsevier. (2013). Nutrición comunicaría. España: <em>MASSON.</em>',
+    },
+    {
+      referencia:
+        'Fotolia. <em>Group of multiethnic hands holding world nutrition.</em>',
+    },
+    {
+      referencia:
+        'Fotolia. (2004). <em>Nutrition facts label hanging on a rope.</em> Vector.',
+      link: 'http://co.fotolia.com/id/64693571',
+    },
+    {
+      referencia:
+        'Fotolia. (2004). <em>Silhouette junges Mannes mit Magen-Darm-Trakt.</em> Vector.',
+      link: 'http://co.fotolia.com/id/39992078',
+    },
+    {
+      referencia:
+        'Hernández, D. R. (20 de Enero de 2015). Medicina Preventiva Santa Fe.M. y. (1999). Tratado de Nutrición. Madrid, España: Diaz de Santos SA',
+      link:
+        'http://medicinapreventiva.info/generalidades/10231/conozca-el-papel-fundamental-de-las-proteinas-para-la-salud-por-linternista/ ',
+    },
+    {
+      referencia:
+        '(2013). Manual de Nutrición y Dietetica. En A. Carbajal Azcona. Marid.',
+      link:
+        'http://eprints.ucm.es/22755/1/Manual-nutricion-dietetica-CARBAJAL.pdf',
+    },
+    {
+      referencia:
+        'Mataiz, J. (2002). Nutrición y Alimentación. Madrid, España: Océano.',
+    },
+    {
+      referencia:
+        'Nutrición deportiva. (s.f.). Servicio de nutrición. Pamplona, Navarra, España',
+      link: 'http://www.podiumns.com/es/servicios/nutricion-deportiva/.',
+    },
+    {
+      referencia:
+        'Rodriguez, V. y. (2008). Bases de la Alimentación Humana. La Coruña, España: <em>Gesbiblo.</em>',
+    },
+    {
+      referencia:
+        'UNED. Facultad de Ciencias. Nutrición y Dietética. (2017). Guía de Alimentación y Salud. Obtenido del valor energético de los alimentos: Las necesidades energéticas del ser humano:',
+      link:
+        'http://www2.uned.es/pea-nutricion-y-dietetica-I/guia/guia_nutricion/valor_necesidades.htm',
+    },
+    {
+      referencia:
+        'Vasquez, C. c. (2005). Alimentación y Nutrición: Manual teórico Práctico. Madrid, España: Ediciones Diaz de Santos SA',
     },
   ],
   creditos: [
@@ -152,9 +340,34 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+          nombre: 'Diana cristina muñoz urrea',
+          cargo: 'Experta temática',
+          centro: 'Regional QuindíoCentro Agroindustrial',
+        },
+        {
+          nombre: 'Luis Guillermo Roberto baez',
+          cargo: 'Diseñadores gráficos',
+          centro: 'Regional Quindío Centro Agroindustrial',
+        },
+        {
+          nombre: 'Zulma rocio bravo Jiménez',
+          cargo: 'Diseñadores gráficos',
+          centro: 'Regional Quindío Centro Agroindustrial',
+        },
+        {
+          nombre: 'Yuri Alexandra baez',
+          cargo: 'Asesora pedagógica',
+          centro: 'Regional Quindío Centro Agroindustrial',
+        },
+        {
+          nombre: 'Hector salina castellanos',
+          cargo: 'Guionista del equipo',
+          centro: 'Regional Quindío Centro Agroindustrial.',
+        },
+        {
+          nombre: 'Zulma Yurany viancha rodriguez',
+          cargo: 'Líder del del Equipo ',
+          centro: 'Regional Quindío Centro Agroindustrial.',
         },
       ],
     },
--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,47 +15,237 @@ export default {
   components: { Actividad },
   data: () => ({
     cuestionario: {
-      tema: 'Nombre del componente formativo',
-      titulo: 'Cuestionario',
-      introduccion: '<b> Objetivo:</b> Texto de Word',
-      barajarPreguntas: false,
+      tema:
+        'Importancia de la nutrición durante la vida para evitar enfermedades',
+      titulo:
+        'Importancia de la nutrición durante la vida para evitar enfermedades.',
+      introduccion:
+        'Preguntas de opción múltiple: lea los siguientes enunciados y seleccione la respuesta que considere correcta.',
+      barajarPreguntas: true,
       preguntas: [
         {
           id: 1,
-          texto: 'Texto pregunta 1 con <strong>Negrita</strong> <em>Itálica</.',
+          texto:
+            '¿Cuál de las siguientes enfermedades es comúnmente causada por la inadecuada alimentación?',
           imagen: require('@/assets/componentes/pregunta_1.svg'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: 'Opción 1', esCorrecta: true },
-            { id: 'b', texto: 'Opción 2', esCorrecta: false },
-            { id: 'c', texto: 'Opción 3', esCorrecta: false },
-            { id: 'd', texto: 'Opción 4', esCorrecta: false },
+            { id: 'a', texto: 'Artritis.', esCorrecta: false },
+            { id: 'b', texto: 'Diabetes tipo 2.', esCorrecta: true },
+            { id: 'c', texto: 'Asma', esCorrecta: false },
+            { id: 'd', texto: 'Hipertensión arterial', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
         },
         {
           id: 2,
-          texto: 'Texto pregunta 2',
+          texto:
+            '¿Qué aspecto es fundamental para evitar enfermedades por malnutrición?',
           imagen: require('@/assets/componentes/pregunta_2.png'),
           barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: 'Falso', esCorrecta: true },
-            { id: 'b', texto: 'Verdadero', esCorrecta: false },
+            {
+              id: 'a',
+              texto: 'Evitar las grasas a toda costa',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Seguir una dieta equilibrada',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
         },
         {
           id: 3,
-          texto: 'Texto pregunta 3',
-          imagen: require('@/assets/componentes/pregunta_3.png'),
-          barajarRespuestas: false,
+          texto: '¿Por qué es crucial la nutrición durante la infancia?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
           opciones: [
-            { id: 'a', texto: 'Opción 1', esCorrecta: true },
-            { id: 'b', texto: 'Opción 2', esCorrecta: false },
-            { id: 'c', texto: 'Opción 3', esCorrecta: false },
-            { id: 'd', texto: 'Todas las anteriores', esCorrecta: false },
+            {
+              id: 'a',
+              texto: 'Para desarrollar hábitos poco saludables',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Para un crecimiento y desarrollo óptimos',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Para aumentar el tiempo de juego',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Para disminuir la actividad física',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 4,
+          texto:
+            '¿Cuál es un beneficio de una buena alimentación en la adultez',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Prevención de enfermedades crónicas',
+              esCorrecta: true,
+            },
+            { id: 'b', texto: 'Mayor consumo de azúcares', esCorrecta: false },
+            {
+              id: 'c',
+              texto: 'Ignorar la actividad física',
+              esCorrecta: false,
+            },
+            { id: 'd', texto: 'La historia familiar', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 5,
+          texto: '¿Qué factor afecta las necesidades nutricionales en la vejez',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'La cantidad de horas de sueño',
+              esCorrecta: false,
+            },
+            { id: 'b', texto: 'Cambios en el metabolismo', esCorrecta: true },
+            {
+              id: 'c',
+              texto: 'La cantidad de ejercicio realizado en la juventud',
+              esCorrecta: false,
+            },
+            { id: 'd', texto: 'Mejorar la visión', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 6,
+          texto:
+            '¿Qué tipo de alimentos son esenciales para prevenir la desnutrición?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Alimentos ricos en azúcares',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Alimentos ricos en nutrientes',
+              esCorrecta: true,
+            },
+            { id: 'c', texto: 'Comida rápida', esCorrecta: false },
+            { id: 'd', texto: 'Alimentos muy procesados', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 7,
+          texto:
+            '¿Cuál de los siguientes es un signo de mala nutrición en niños?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            { id: 'a', texto: 'Participación en deportes', esCorrecta: false },
+            { id: 'b', texto: 'Bajo peso para su edad', esCorrecta: true },
+            { id: 'c', texto: 'Consumo de verduras', esCorrecta: false },
+            { id: 'd', texto: 'Buen rendimiento escolar', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 8,
+          texto: '¿Qué se recomienda para mejorar la salud en adolescentes?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Incluir una variedad de alimentos en la dieta',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Consumir solo alimentos ultra procesados',
+              esCorrecta: false,
+            },
+            { id: 'c', texto: 'Saltar el desayuno', esCorrecta: false },
+            {
+              id: 'd',
+              texto: 'Aumentar el consumo de refrescos',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 9,
+          texto: '¿Cómo influyen los factores ambientales en la nutrición?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            { id: 'a', texto: 'No tienen impacto', esCorrecta: false },
+            {
+              id: 'b',
+              texto: 'Pueden afectar el acceso a alimentos saludables',
+              esCorrecta: false,
+            },
+            {
+              id: 'c',
+              texto: 'Solo influyen en la salud mental',
+              esCorrecta: true,
+            },
+            {
+              id: 'd',
+              texto: 'Son irrelevantes para el bienestar físico',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 10,
+          texto: '¿Qué es un aspecto clave de la educación nutricional?',
+          imagen: require('@/assets/componentes/pregunta_2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            { id: 'a', texto: 'Promover dietas extremas', esCorrecta: false },
+            {
+              id: 'b',
+              texto: 'Fomentar elecciones alimentarias saludables',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'Ignorar las necesidades individuales',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'Enfatizar el consumo de alimentos altos en azúcar',
+              esCorrecta: false,
+            },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
