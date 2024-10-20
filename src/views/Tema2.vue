845b64940a482710cbe856e2fb9482c126cbca18
Leoska89 <leoska890@gmail.com>
2024-10-19 22:18:44 -0500

    up

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,151 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Aspectos fundamentales para evitar enfermedades por malnutrición
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-5.col-md-6.mb-md-5
+        figure
+          img(src='@/assets/curso/temas/tema2/img-1.png')           
+          
+      .col-lg-7
+        .row.mb-3.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2      
+        p.mb-3 La buena alimentación con una dieta equilibrada es aquella que contiene una cantidad adecuada y variada de alimentos que proporcionan los nutrientes con las cualidades necesarias y en las cantidades correctas.
+        p.mb-3 El funcionamiento normal del cuerpo humano requiere de la presencia de sustancias imprescindibles que por sí mismo no se pueden producir. Son nutrientes que influyen directamente en el funcionamiento del cuerpo desde el momento en que se ingieren, e incluso tiene incidencia en el funcionamiento futuro del cuerpo de la persona. Estas sustancias son las siguientes: proteínas, glúcidos, lípidos, vitaminas, sales minerales y agua.
+        p.mb-3 Adicionalmente a estas sustancias básicas de una dieta equilibrada, existen otras que resultan muy beneficiosas para el cuerpo humano por sus distintas propiedades y los efectos que producen en las personas que los consumen. Entre ellas están los antioxidantes y los componentes bioactivos.
+
+    .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+      .col-auto.pe-2
+        figure
+          img(src='@/assets/curso/temas/tema2/img-4.svg')
+      .col-auto.px-0
+        h3.mb-0 Alimentación balanceada rica en antioxidantes
+
+    .row.justify-content-center.align-items-center(data-aos="fade-left")
+      .col-lg-12.mb-4
+        p El cuerpo humano sufre constantemente ataques por unas sustancias muy dañinas que reciben el nombre de radicales libres, estos son moléculas con un alto grado de inestabilidad que se forman en el cuerpo de manera natural a causa del proceso de oxidación, sin embargo, su presencia puede destruir las células, los tejidos e incluso lo órganos de una persona. 
+        p Para combatir la acción de los radicales libres en el cuerpo se recurre a sustancias que contrarresten sus efectos, entre ellas están los llamados antioxidantes que son moléculas con la capacidad de prevenir o retardar el proceso de oxidación de otras moléculas, son sustancias fitoquímicas, lo que quiere decir que se originan en las plantas. Corresponden a algunas vitaminas y nutrientes, por eso la mayor fuente de antioxidantes son las frutas y verduras. Estas sustancias se encuentran en alimentos como el olivo, el ajo, el arroz integral, las fresas, los arándanos, las espinacas, las nueces del café, la coliflor, el brócoli, el jengibre, el perejil, la cebolla, los cítricos, la semolina, los tomates, el té, el romero, entre muchos otros.
+     
+    .row.justify-content-center.align-items-center.mb-3(data-aos="fade-left")
+      .col-lg-4.col-md-6
+        figure.mb-4
+          img(src='@/assets/curso/temas/tema2/img-3.svg') 
+      .col-lg-8 
+        .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2
+            figure
+              img(src='@/assets/curso/temas/tema2/img-4.svg')
+          .col-auto.px-0
+            h3.mb-0 Actividad física y dieta deportiva
+        p En sentido estrictamente nutricional en cuanto al deporte, se debe tener siempre presente que las sustancias que absorbe el cuerpo en el día a día a través de la alimentación producen la energía que se requiere para el mantenimiento de las funciones orgánicas que se efectúan durante la actividad física, tanto en los momentos de reposo como en los de movimiento corporal.
+        p La energía permite la realización de trabajos fisiológicos; esta se presenta en forma de glúcidos, lípidos y prótidos, sin embargo, para su extracción y utilización óptima de la energía contenida en estas sustancias, se requiere de la presencia de otros nutrientes como lo son las vitaminas y los minerales. Estos juegan un papel fundamental en la transferencia de energía al cuerpo.
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-10
+        .row.align-items-center
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 4.
+            span Importancia del deporte.
+
+          .col-lg-12 
+            .col-auto.pe-2
+              figure
+                img(src='@/assets/curso/temas/tema2/img-5.png')                 
+              .tabla-b.color-acento-contenido.mb-5.tbl
+                table
+                  caption Nota. SENA    
+
+    .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+      .col-auto.pe-2
+        figure
+          img(src='@/assets/curso/temas/tema2/img-4.svg')
+      .col-auto.px-0
+        h3.mb-0 Determinar las necesidades Nutritivas
+    .row.justify-content-center.align-items-center(data-aos="fade-left")
+      .col-lg-12.mb-4
+        p Los músculos son el motor para cualquier movimiento deportivo porque con su acción de contracción y retracción permiten la ejecución de las órdenes que el sistema nervioso les envía. Para la contracción, la célula muscular requiere de energía que se le suministra a través de los nutrientes energéticos como los carbohidratos y los lípidos. La presencia de proteínas también es fundamental por el papel estructural de la fisiología de músculo.
+        p En un principio, los requisitos energéticos de una persona que realiza deporte de manera regular se hallan en la alimentación habitual de cualquier persona, con variaciones según diferentes factores geográficos, climáticos o económicos, pero cercanos siempre a los siguientes:        
+      .col-lg-10 
+          .col-auto.pe-2
+            figure
+              img(src='@/assets/curso/temas/tema2/img-6.svg')
+
+    .row.justify-content-center.align-items-center.mb-3(data-aos="fade-left")
+      .col-lg-4.col-md-6
+        figure.mb-4
+          img(src='@/assets/curso/temas/tema2/img-7.png') 
+      .col-lg-8 
+        .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2            
+          .col-auto.px-0
+            p El gasto energético de un deportista tiene unas necesidades particulares, sin embargo, existen otras necesidades calóricas a las que se demanda por la acción del deporte. Estas pueden establecerse en unas 2500 y 3500 kcal/día y se derivan del gasto normal realizado por el cuerpo en las actividades regulares deltrabajo, el crecimiento y el desarrollo del cuerpo como la propia combustión. A estos gastos se les añaden aquellos propios de la actividad deportiva, tanto por su cantidad y su calidad en los siguientes aspectos:
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-10
+        .row.align-items-center
+          .titulo-sexto.color-acento-contenido
+            h5 Figura 5
+
+        .col-lg-12 
+          .col-auto.pe-2
+            figure
+              img(src='@/assets/curso/temas/tema2/img-8.svg')                 
+            .tabla-b.color-acento-contenido.mb-5.tbl
+              table
+                caption Nota. SENA 
+            p Consecuente con lo anterior, es posible considerar que, en función de las horas de práctica deportiva, el gasto diario de energía de un deportista se incrementará a un rango de 3500 y 4500 kcal/kg/hora e incluso más.
+            p A una persona que realiza deporte se le recomienda tener en cuenta los siguientes consejos básicos:    
+
+        .row.justify-content-center.align-items-center(data-aos="zoom-in")
+          .col-xl-6.col-lg-6.col-md-8
+            .crd_02.mb-5
+                .crd_02__icon.dan
+                  img(src="@/assets/curso/temas/tema2/img-9.svg", alt="alt")          
+                .crd_02__txt
+                  p.mb-1 Ingerir comida equilibrada, variada, bien condimentada y cocinada, además establecer horarios fijos de comida sin mezclar con otras actividades.
+                  
+          .col-xl-6.col-lg-6.col-md-8
+            .crd_02.mb-5
+                .crd_02__icon.dan
+                  img(src="@/assets/curso/temas/tema2/img-9.svg", alt="alt")          
+                .crd_02__txt
+                  p.mb-1 Manejar una excelente masticación y mantenerse hidratado constantemente.
+                  
+          .col-xl-6.col-lg-6.col-md-8
+            .crd_02.mb-5
+                .crd_02__icon.dan
+                  img(src="@/assets/curso/temas/tema2/img-9.svg", alt="alt")          
+                .crd_02__txt
+                  p.mb-1 Antes de realizar una actividad deportiva extensa se debe permitir un espacio de tres horas de reposo después de ingerir alimento.
+          .col-xl-6.col-lg-6.col-md-8
+            .crd_02.mb-5
+                .crd_02__icon.dan
+                  img(src="@/assets/curso/temas/tema2/img-9.svg", alt="alt")          
+                .crd_02__txt
+                  p.mb-1 Respetar los diferentes periodos de la vida deportiva: el entrenamiento, la competición y la recuperación.
+    .row.justify-content-center.align-items-center.mb-3(data-aos="fade-left")
+      .col-lg-4.col-md-6
+        figure.mb-4
+          img(src='@/assets/curso/temas/tema2/img-10.png') 
+      .col-lg-8 
+        .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2            
+          .col-auto.px-0
+            p Adicionalmente un deportista debe tener en cuenta los siguientes aspectos que varían según las características de cada persona:
+            p Comer únicamente la cantidad requerida según el apetito de cada quien, alimentarse en exceso puede revocardesórdenes digestivos.
+            p Según los hábitos alimenticios y los gustos del deportista, unos alimentos son aceptados y otros rechazados, así como también hay alimentos que son tolerados por el organismo de la persona, por lo cual debe tenerse un nivel de consciencia cuando se escoge lo que se debe ingerir.
+            p La cantidad de alimento a ingerir depende de las necesidades cuantitativas y cualitativas de cada individuo.
+            p El deportista tiene que determinar su valor de saciedad, conocer en qué momento debe parar de comer.
+            p Las necesidades calóricas que se generan por el esfuerzo deportivo, con el tiempo se autorregulan por el propio organismo debido a la adaptación generada por las costumbres alimenticias de las personas.
+ 
+
+    
+
+   
+         
+  
+
+
 
 </template>
 
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,77 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Importancia de la nutrición durante la vida
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-5.col-md-6.mb-md-5
+        figure
+          img(src='@/assets/curso/temas/tema3/img-1.png')           
+          
+      .col-lg-7
+        .row.mb-3.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2      
+        p.mb-3 Este recurso constituye una herramienta práctica para entender cada una de las etapas de la vida y considerar el aspecto nutricional como parte fundamental en cada una de las etapas para mantenerse saludable. 
+    .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+      .col-auto.pe-2
+        figure
+          img(src='@/assets/curso/temas/tema2/img-4.svg')
+      .col-auto.px-0
+        h3.mb-0 Una dieta equilibrada
+
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-3.col-md-6.mb-md-5
+        figure
+          img(src='@/assets/curso/temas/tema3/img-2.png')           
+          
+      .col-lg-9
+        .row.mb-3.pe-lg-3.align-items-center(data-aos="fade-left")
+          .col-auto.pe-2      
+        p Una dieta equilibrada debe cumplir con cinco aspectos básicos:
+        p <b>Calorías suficientes:</b> Aportar las calorías necesarias para que el organismo realice de forma eficiente los procesos metabólicos y trabajos físicos.
+        p <b>Nutrientes suficientes:</b> Suministrar nutrientes al organismo como proteínas, vitaminas y minerales para que realicen las funciones plásticas y reguladoras.
+        p <b>Agua:</b> Es necesario incluir en la dieta un mínimo de tres litros de agua al día; este elemento, aunque no es un nutriente, desempeña en el organismo procesos de asimilación y eliminación del sistema celular. 
+        p <b>Fibra dietaría:</b> Debe suministrarse en cantidades suficientes porque esta aporta ligninas, polisacáridos y oligosacáridos.
+        p <b>Equilibrio de los nutrientes:</b> Todos los aportes calóricos deben estar equilibrados entre sí, según lo establecido por los expertos de la Organización de las Naciones Unidas para la Alimentación y la Agricultura (FAO) y la Organización Mundial de la Salud (OMS). 
+       
+    .row.mb-4.pe-lg-3.align-items-center(data-aos="fade-left")
+      .col-auto.pe-2
+        figure
+          img(src='@/assets/curso/temas/tema2/img-4.svg')
+      .col-auto.px-0
+        h3.mb-5 Criterios a tener en cuenta en la ingesta diaria recomendada (RDA)
+    
+    .row.justify-content-center.align-items-center.mb-5(data-aos="fade-right")
+      .col-lg-9.order-lg-1.order-2
+        p Consumir todos los grupos de alimentos de forma variada, los glúcidos en un 55% a 60% y aumentar la cantidad de acuerdo con el ingreso energético total, sin sobrepasar un 10% en consumo de azúcares simples (azúcar refinada).
+        p Se recomienda disminuir el consumo de lípidos en un 30% con respecto al ingreso energético total, también reducir el colesterol en 300 mg/día.
+        p Controlar el consumo de proteínas siempre y cuando el consumo de calorías sea el adecuado.
+        p Consumir fibra dietaría a diario, su ingesta no debe ser inferior a 22 gr/día.
+        p Controlar el consumo de sal en 3 gr/ día para evitar el exceso de sodio en el cuerpo.
+
+
+      .col-lg-3.col-md-6.order-lg-2.order-1.mb-3        
+        figure
+          img(src='@/assets/curso/temas/tema3/img-3.png')    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-6
+            p slslslls
+          .col-lg-6
+            p ksksksk
+
+
+
+
+
+
+
+       
+
+     
+
+
 
 </template>
 
